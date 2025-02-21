357d233703810ae713e387e26d064be97bc97274
jzumetae <jzumetae@miumg.edu.gt>
2025-02-21 09:52:14 -0600

    Update laboratorioviernes.cpp

--- laboratorioviernes/laboratorioviernes.cpp
+++ laboratorioviernes/laboratorioviernes.cpp
@@ -3,24 +3,56 @@
 
 using namespace std;
 
-class Libro {
-protected:
-    string nombre, autor;
-    int anio;
+class libro {
+private:
+	string nombre;
+	string autor;
+	int anio;
 
 public:
-    Libro(string m, string aut, int ani) : nombre(m), autor(aut), anio(ani) {}
-    void mostrarLibro() {
-        cout << "--Detalles del libro: --\n" << "Nombre: " << nombre << "\n Autor: " << autor << "\n Anio: " << anio << endl;
-    }
+	libro(string _nombre, string _autor, int _anio) {
+		nombre = _nombre;
+		autor = _autor;
+		anio = _anio;
+
+	}
+
+	void mostrarDatos() {
+		cout << "nombre: " << nombre << ", autor: " << autor << ", anio: " << anio << endl; 
+	}
+};
+// clase hija 
+class cuento : public libro {
+private:
+	int paginas;
+
+public:
+	cuento(string _nombre, string _autor, int _anio, int _paginas) :
+		libro(_nombre, _autor, _anio) {
+		paginas = _paginas; 
+	}
+
+	void mostrarCuantasPaginas() {
+		mostrarDatos();
+		cout << "cuanta paginas tiene el cuento: " << paginas << endl;
+	}
 };
 
 int main() {
+	string nombre, autor;
+	int anio, paginas;
 
-    Libro librito("El Quijote de la Mancha", "Miguel de Cervantes", 2008);
-    librito.mostrarLibro();
-    
-    return 0;
+	cout << "ingrese el nombre del cuento: ";
+	cin >> nombre;
+	cout << "ingrese el anio de publicacion: ";
+	cin >> anio;
+	cout << "ingrese el nombre del autor: ";
+	cin >> autor;
+	cout << "ingrese el numero de pagians del ciento: ";
+	cin >> paginas;
+
+	cuento micuento(nombre, autor, anio, paginas);
+
+	micuento.mostrarCuantasPaginas();
+	return 0;
 }
-
-
