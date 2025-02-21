e3ed172d2709aeb21e9a23fabc80a263929bb462
jzumetae <jzumetae@miumg.edu.gt>
2025-02-21 09:52:48 -0600

    Update laboratorioviernes.cpp

--- laboratorioviernes/laboratorioviernes.cpp
+++ laboratorioviernes/laboratorioviernes.cpp
@@ -3,56 +3,98 @@
 
 using namespace std;
 
-class libro {
-private:
-	string nombre;
-	string autor;
-	int anio;
+class Libro {
+protected:
+    string nombre, autor;
+    int anio;
 
 public:
-	libro(string _nombre, string _autor, int _anio) {
-		nombre = _nombre;
-		autor = _autor;
-		anio = _anio;
-
-	}
-
-	void mostrarDatos() {
-		cout << "nombre: " << nombre << ", autor: " << autor << ", anio: " << anio << endl; 
-	}
+    Libro(string m, string aut, int ani) : nombre(m), autor(aut), anio(ani) {}
+    void mostrarLibro() {
+        cout << "--Detalles del libro: --\n" << "Nombre: " << nombre << "\n Autor: " << autor << "\n Anio: " << anio << endl;
+    }
 };
-// clase hija 
-class cuento : public libro {
+
+class Ensayo : public Libro {
 private:
-	int paginas;
+    string tipoensayo;
 
 public:
-	cuento(string _nombre, string _autor, int _anio, int _paginas) :
-		libro(_nombre, _autor, _anio) {
-		paginas = _paginas; 
-	}
+    Ensayo(string _nombre, string _autor, int _ano, string _tipoensayo)
+        : Libro(_nombre, _autor, _ano), tipoensayo(_tipoensayo) {}
+    void mostrartipoensayo() {
+        mostrarLibro();
+        cout << "Tipo de ensayo: " << tipoensayo << endl;
+    }
+};
 
-	void mostrarCuantasPaginas() {
-		mostrarDatos();
-		cout << "cuanta paginas tiene el cuento: " << paginas << endl;
-	}
+class TextoEscolar : public Libro {
+private:
+    string editorial;
+
+public:
+    TextoEscolar(string nom, string au, int a, string ed) : Libro(nom, au, a), editorial(ed) {}
+    void mostrarinfo() {
+        mostrarLibro();
+        cout << "Editorial: " << editorial << endl;
+    }
+};
+
+class Biografia : public Libro {
+private:
+    string personaje;
+
+public:
+    Biografia(string m, string aut, int ani, string per) : Libro(m, aut, ani), personaje(per) {}
+    void mostrarBiografia() {
+        mostrarLibro();
+        cout << "Personaje Célebre: " << personaje << endl;
+    }
+};
+
+class Novela : public Libro {
+private:
+    string tipo;
+
+public:
+    Novela(string n, string a, int o, string t) : Libro(n, a, o), tipo(t) {}
+    void mostrarinfo() {
+        mostrarLibro();
+        cout << "Tipo de libro: " << tipo << endl;
+    }
+};
+
+class Cuento : public Libro {
+private:
+    int paginas;
+
+public:
+    Cuento(string _nombre, string _autor, int _anio, int _paginas) : Libro(_nombre, _autor, _anio), paginas(_paginas) {}
+    void mostrarCuantasPaginas() {
+        mostrarLibro();
+        cout << "Páginas: " << paginas << endl;
+    }
 };
 
 int main() {
-	string nombre, autor;
-	int anio, paginas;
+    Libro librito("El Quijote de la Mancha", "Miguel de Cervantes", 2008);
+    librito.mostrarLibro();
 
-	cout << "ingrese el nombre del cuento: ";
-	cin >> nombre;
-	cout << "ingrese el anio de publicacion: ";
-	cin >> anio;
-	cout << "ingrese el nombre del autor: ";
-	cin >> autor;
-	cout << "ingrese el numero de pagians del ciento: ";
-	cin >> paginas;
+    Ensayo miEnsayo("Ensayo sobre la ceguera", "José Saramago", 1995, "Filosófico");
+    miEnsayo.mostrartipoensayo();
 
-	cuento micuento(nombre, autor, anio, paginas);
+    TextoEscolar miTexto("Matemáticas Avanzadas", "Luis Pérez", 2015, "Santillana");
+    miTexto.mostrarinfo();
 
-	micuento.mostrarCuantasPaginas();
-	return 0;
+    Biografia bio("La vida de Chespirito", "El hijo de chespirito", 2010, "Chespirito");
+    bio.mostrarBiografia();
+
+    Novela novela1("Cien años de soledad", "Gabriel García Márquez", 1967, "Ficción");
+    novela1.mostrarinfo();
+
+    Cuento micuento("El Principito", "Antoine de Saint-Exupéry", 1943, 96);
+    micuento.mostrarCuantasPaginas();
+
+    return 0;
 }
+
